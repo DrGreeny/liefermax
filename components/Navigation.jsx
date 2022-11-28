3e992e00c3c21f14ece8dffcd72c6c7e448f5b2e
Stefan <s.timter@gmx.de>
2022-11-28 08:42:11 +0100

    initial commit of project

---
+++ components/Navigation.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Badge } from "react-bootstrap";
+
+export default function Navigation() {
+  return (
+    <div className="shadow sticky-top p-2 mb-2 bg-danger">
+      <div className="d-flex justify-content-between align-items-center">
+        <Link href="/">
+          <Image src={"/Bilder/logo.png"} alt="logo" width={180} height={75} />
+        </Link>
+
+        <Link href="/warenkorb">
+          <Image
+            src={"/Bilder/warenkorb.png"}
+            alt="logo"
+            width={30}
+            height={30}
+          />
+          <Badge pill bg="success">
+            2
+          </Badge>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ pages/warenkorb.jsx
@@ -0,0 +1,7 @@
+export default function Warenkorb() {
+  return (
+    <div>
+      <h1>Warenkorb</h1>
+    </div>
+  );
+}
